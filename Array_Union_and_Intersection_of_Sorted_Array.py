a39d66c74c2c7693f305ce42c4d2ad3291262e4e
AshTiwari <ashu.atiwa@gmail.com>
2020-12-27 17:54:44 +0530

    Using merge function of merge sort.

--- Array_Union_and_Intersection_of_Sorted_Array.py
+++ Array_Union_and_Intersection_of_Sorted_Array.py
@@ -6,13 +6,16 @@ def Union(sorted_array_1,sorted_array_2):
 	array = []
 	while(i < len(sorted_array_1) and j < len(sorted_array_2)):
 		if sorted_array_1[i] < sorted_array_2[j]:
-			array.append(sorted_array_1[i])
+			if len(array) == 0 or sorted_array_1[i] != array[-1]:
+				array.append(sorted_array_1[i])
 			i = i+1
 		elif sorted_array_1[i] > sorted_array_2[j]:
-			array.append(sorted_array_2[j])
+			if len(array) == 0 or sorted_array_2[j] != array[-1]:
+				array.append(sorted_array_2[j])
 			j = j + 1
 		elif sorted_array_1[i] == sorted_array_2[j]:
-			array.append(sorted_array_1[i])
+			if len(array) == 0 or sorted_array_1[i] != array[-1]:
+				array.append(sorted_array_1[i])
 			i = i+1
 			j = j + 1
 	if i == len(sorted_array_1) :
@@ -28,7 +31,8 @@ def Intersection(sorted_array_1,sorted_array_2):
 	array = []
 	while(i < len(sorted_array_1) and j < len(sorted_array_2)):
 		if sorted_array_1[i] == sorted_array_2[j]:
-			array.append(sorted_array_1[i])
+			if len(array) == 0 or sorted_array_1[i] != array[-1]:
+				array.append(sorted_array_1[i])
 			i = i+1
 			j = j + 1
 	
@@ -40,6 +44,6 @@ def Intersection(sorted_array_1,sorted_array_2):
 
 if __name__ == "__main__":
 	sorted_array_1 = [1,1,3,5,7]
-	sorted_array_2 = [0,1,2,4,6,7]
+	sorted_array_2 = [0,1,1,2,4,6,7]
 	print(Union(sorted_array_1,sorted_array_2))
 	print(Intersection(sorted_array_1,sorted_array_2))
