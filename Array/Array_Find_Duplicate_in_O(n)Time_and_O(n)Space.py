3ec3d79fe28e1a64f8633321d7d2ccedbf309520
AshTiwari <ashu.atiwa@gmail.com>
2021-01-26 11:30:00 +0530

    Using algebraic function method for all kind of series.

--- Array/Array_Find_Duplicate_in_O(n)Time_and_O(n)Space.py
+++ Array/Array_Find_Duplicate_in_O(n)Time_and_O(n)Space.py
@@ -1,4 +1,5 @@
 # Return Duplicate elements in an Array with O(n) Time and O(n) Space.
+# This code considers all element of array to be positive, however the code can be simply modified for negative numbers.
 
 def updateFunctAndDuplicate(funct,duplicate,x):
 	temp = funct
@@ -22,3 +23,8 @@ if __name__ =="__main__":
 	array = [1,2,3,1,2,1,5,6,0,0,0]
 #	array = [1]
 	createFunction(array)
+
+# this code contains 'duplicate' string to store the duplicate numbers.
+# it helps in preventing to print the same duplicates twice.
+# However, for a really large value of n, this string can be very large.
+# Then, we can remove the string but the code will then also print every repetition of duplicate element.
