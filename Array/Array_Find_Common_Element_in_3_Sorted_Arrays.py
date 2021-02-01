ca5784be9dba65e341341ab1661802f24f144ec7
AshTiwari <ashu.atiwa@gmail.com>
2021-02-01 20:15:22 +0530

    Later enhance Code by using binarySearch

---
+++ Array/Array_Find_Common_Element_in_3_Sorted_Arrays.py
@@ -0,0 +1,30 @@
+# Find commmon elements in three sorted arrays.
+
+def threePointer(a,ar,arr):
+	a_index = ar_index = arr_index = 0
+	duplicate = []
+	while(a_index < len(a) and ar_index < len(ar) and arr_index < len(arr)):
+		maxElement = max(a[a_index],ar[ar_index],arr[arr_index])
+		while(a_index < len(a) and a[a_index] < maxElement ):
+			a_index += 1
+		while(ar_index < len(ar) and ar[ar_index] < maxElement ):
+			ar_index += 1
+		while(arr_index < len(arr) and arr[arr_index] < maxElement):
+			arr_index += 1
+		if not (a_index < len(a) and ar_index < len(ar) and arr_index < len(arr)):
+			break
+		if a[a_index] == ar[ar_index] == arr[arr_index]:
+			#print(f"{maxElement}", end = " ")
+			duplicate.append(maxElement)
+			a_index += 1
+			ar_index += 1
+			arr_index += 1
+	return duplicate	
+
+if __name__ == "__main__":
+	a = [1, 5]
+	ar = [3, 4, 5, 5, 10]
+	arr = [5, 5, 10, 20]
+	print(threePointer(a,ar,arr))
+
+	
